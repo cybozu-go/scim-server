9c003bbed004511e7c989d892738b1aebc421098
Daisuke Maki <lestrrat+github@gmail.com>
2022-08-15 22:30:56 +0900

    start using schema information in code generation

--- tools/cmd/genent/go.mod
+++ tools/cmd/genent/go.mod
@@ -3,6 +3,7 @@ module github.com/cybozu-go/scim-server/tools/cmd/genent
 go 1.18
 
 require (
+	github.com/cybozu-go/scim v0.0.0-20220815132929-d9844e6c8fc9
 	github.com/goccy/go-yaml v1.9.5
 	github.com/lestrrat-go/codegen v1.0.4
 	github.com/lestrrat-go/runcmd v0.0.0-20220203052504-ca4ca4ca6b50
@@ -12,10 +13,10 @@ require (
 require (
 	github.com/fatih/color v1.10.0 // indirect
 	github.com/lestrrat-go/option v1.0.0 // indirect
-	github.com/mattn/go-colorable v0.1.8 // indirect
-	github.com/mattn/go-isatty v0.0.12 // indirect
-	golang.org/x/mod v0.3.0 // indirect
-	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
-	golang.org/x/tools v0.0.0-20200918232735-d647fc253266 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/mod v0.5.1 // indirect
+	golang.org/x/sys v0.0.0-20211205182925-97ca703d548d // indirect
+	golang.org/x/tools v0.1.9-0.20211216111533-8d383106f7e7 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- tools/genent.sh
+++ tools/genent.sh
@@ -1,9 +1,50 @@
 #!/bin/bash
 
+# Because we want to be able to use any arbitrary version of the
+# github.com/cybozu-go/scim module when running this script,
+# we _DYNAMICALLY_ modify our requirements when needed.
+
+# We need to do this because we need both the actual Go module
+# and the helper file located in tools/cmd/genresources/objects.yaml
+
+set -e
+
+GENENT_TEMP=.genent-tmp
+mkdir $GENENT_TEMP
+GENENT_TEMP=$(cd $GENENT_TEMP; pwd -P) # absolute path
+
+function cleanup() {
+	echo "Cleaning up..."
+	if [[ -e "$GENENT_TEMP" ]]; then
+		rm -rf $GENENT_TEMP
+	fi
+}
+
+trap cleanup EXIT
+
+# Setup the SCIM directory
+if [[ -z "$SCIM_DIR" ]]; then
+	SCIM_DIR="$GENENT_TEMP/scim"
+	git clone https://github.com/cybozu-go/scim $SCIM_DIR
+fi
+
+SCIM_DIR=$(cd $SCIM_DIR; pwd -P)
+
 pushd tools/cmd/genent
-go build -o .genent main.go
+
+ABS=$(pwd -P)
+
+# Here comes the absolutely bad bad part
+# First, make a copy of go.mod so that we can revert it back
+# when required
+cp go.mod "$GENENT_TEMP/go.mod"
+
+go mod edit -replace=github.com/cybozu-go/scim=$(realpath --relative-to="$ABS" $SCIM_DIR)
+go mod tidy
+go build -o "$GENENT_TEMP/genent" main.go
+go mod edit -dropreplace=github.com/cybozu-go/scim
+go mod tidy
 popd
 
-./tools/cmd/genent/.genent $GENENT_ARGS
-rm ./tools/cmd/genent/.genent
+$GENENT_TEMP/genent -clone-dir=$SCIM_DIR
 
